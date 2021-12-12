1c31cf49e3c0cc523511d3e3e369554fa2cba94d
Baboo Bhaiya <baboobhaiya@protonmail.com>
2021-12-12 11:56:02 +0000

    update v2 reserves

--- apr.py
+++ apr.py
@@ -1,6 +1,7 @@
 import json
 from web3 import Web3
 from utils import (
+    CHEFV2_ADDRESS,
     init_chef,
     init_tlp,
     getReserveInUsdc,
@@ -9,7 +10,7 @@ from utils import (
     getTriUsdcRatio,
 )
 
-lpAddresses = {
+v1_pools = {
     0: "0x63da4DB6Ef4e7C62168aB03982399F9588fCd198",
     1: "0x20F8AeFB5697B77E0BB835A8518BE70775cdA1b0",
     2: "0x03B666f3488a7992b2385B12dF7f35156d7b29cD",
@@ -26,17 +27,17 @@ decimals = 18
 chef = init_chef(w3)
 totalAllocPoint = chef.functions.totalAllocPoint().call()
 triPerBlock = chef.functions.triPerBlock().call()
-triUsdRatio = getTriUsdcRatio(w3) / 10 ** 12
-print(triUsdRatio)
+triUsdcRatio = getTriUsdcRatio(w3)
+print(triUsdcRatio/10**12)
 
 
-for id, address in lpAddresses.items():
-    print("Reached here", address)
+for id, address in v1_pools.items():
+    print("V1 Reached here", address)
     tlp = init_tlp(w3, address)
     poolInfo = chef.functions.poolInfo(id).call()
     assert poolInfo[0].lower() == address.lower()
     allocPoint = poolInfo[1]
-    reserveInUSDC = getReserveInUsdc(w3, tlp)
+    reserveInUSDC = getReserveInUsdc(w3, tlp, triUsdcRatio)
     totalSupply = tlp.functions.totalSupply().call()
     totalStaked = tlp.functions.balanceOf(chef.address).call()
     totalStakedInUSDC = getTotalStakedInUSDC(totalStaked, totalSupply, reserveInUSDC)
@@ -59,44 +60,40 @@ for id, address in lpAddresses.items():
             "totalRewardRate": totalWeeklyRewardRate,
             # "totalWeeklyRewardRate": totalWeeklyRewardRate,
             "allocPoint": allocPoint,
-            "apr": getAPR(triUsdRatio, totalSecondRewardRate, totalStakedInUSDC),
+            "apr": getAPR(triUsdcRatio/10**12, totalSecondRewardRate, totalStakedInUSDC),
             "apr2": 0,
             "chefVersion": "v1",
         }
     )
 
+v2_pools = {
+    0: "0x5eeC60F348cB1D661E4A5122CF4638c7DB7A886e",
+    1: "0xd1654a7713617d41A8C9530Fb9B948d00e162194",
+}
 
-# Chef V2 (Temp)
-data.extend(
-    [
-        {
-            "id": 7,
-            "poolId": 0,
-            "lpAddress": "0x5eeC60F348cB1D661E4A5122CF4638c7DB7A886e",
-            "totalSupply": 0,
-            "totalStaked": 0,
-            "totalStakedInUSD": 0,
-            "totalRewardRate": 0,
-            "allocPoint": 0,
-            "apr": 0,
-            "apr2": 0,
-            "chefVersion": "v2",
-        },
-        {
-            "id": 8,
-            "poolId": 1,
-            "lpAddress": "0xd1654a7713617d41A8C9530Fb9B948d00e162194",
-            "totalSupply": 0,
-            "totalStaked": 0,
-            "totalStakedInUSD": 0,
-            "totalRewardRate": 0,
-            "allocPoint": 0,
-            "apr": 0,
-            "apr2": 0,
-            "chefVersion": "v2",
-        },
-    ]
-)
+for id, address in v2_pools.items():
+    print("V2 Reached here", address)
+    tlp = init_tlp(w3, address)
+    # TODO: implement alloc point logic
+    reserveInUSDC = getReserveInUsdc(w3, tlp, triUsdcRatio)
+    totalSupply = tlp.functions.totalSupply().call()
+    totalStaked = tlp.functions.balanceOf(CHEFV2_ADDRESS).call()
+    totalStakedInUSDC = getTotalStakedInUSDC(totalStaked, totalSupply, reserveInUSDC)
+    data.append(
+            {
+                "id": len(v1_pools) + id,
+                "poolId": id,
+                "lpAddress": address,
+                "totalSupply": totalSupply,
+                "totalStaked": totalStaked,
+                "totalStakedInUSD": totalStakedInUSDC / 10 ** 6,
+                "totalRewardRate": 0,
+                "allocPoint": 0,
+                "apr": 0,
+                "apr2": 0,
+                "chefVersion": "v2",
+            }
+    )
 
 
 with open("datav2.json", "w", encoding="utf-8") as f:
