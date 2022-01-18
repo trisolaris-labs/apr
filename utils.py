c656d98d09e0f0288f463bc8abf12fe6574041e4
Baboo Bhaiya <baboobhaiya@protonmail.com>
2022-01-18 17:12:40 +0000

    ignore insufficient liquidity error

--- utils.py
+++ utils.py
@@ -186,12 +186,21 @@ def getAPR(triUsdRatio, totalRewardRate, totalStakedInUSDC):
 
 @retry((ValueError), delay=10, tries=5)
 def convertFeesForPair(tri_maker, pair, w3, acct):
-    transaction = {
-    'gasPrice': w3.eth.gas_price,
-    'nonce': w3.eth.getTransactionCount(acct.address),
-    }
-    convert_tranasction = tri_maker.functions.convert(pair[0], pair[1]).buildTransaction(transaction)
-    signed = w3.eth.account.sign_transaction(convert_tranasction, acct.key)
-    signed_txn = w3.eth.sendRawTransaction(signed.rawTransaction)
-    txn_hash = signed_txn.hex()
-    return w3.eth.waitForTransactionReceipt(txn_hash, timeout=1200)
+    tri_amount = 0
+    try:
+        transaction = {
+        'gasPrice': w3.eth.gas_price,
+        'nonce': w3.eth.getTransactionCount(acct.address),
+        }
+        convert_tranasction = tri_maker.functions.convert(pair[0], pair[1]).buildTransaction(transaction)
+        signed = w3.eth.account.sign_transaction(convert_tranasction, acct.key)
+        signed_txn = w3.eth.sendRawTransaction(signed.rawTransaction)
+        txn_hash = signed_txn.hex()
+        receipt = w3.eth.waitForTransactionReceipt(txn_hash, timeout=1200)
+        for l in receipt['logs']:
+            if (l['topics'][0].hex() == '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' and l['topics'][2].hex() == "0x000000000000000000000000802119e4e253d5c19aa06a5d567c5a41596d6803"):
+                tri_amount += int(l['data'], 16)
+    except ValueError as e:
+        if str(e).find('INSUFFICIENT_LIQUIDITY_BURNED') == -1:
+            raise e
+    return tri_amount
