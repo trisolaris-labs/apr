3fb370b5340dbcd2aaeddb0bdbdaa309a6bca35d
specialist0x <specialist0x@protonmail.com>
2022-04-21 11:12:55 -0700

    Updated python runtime to 3.9

--- deploy_gcc_calculate_tri_xtri_twap.sh
+++ deploy_gcc_calculate_tri_xtri_twap.sh
@@ -2,7 +2,7 @@
 
 gcloud functions deploy gcc_calculate_tri_xtri_twap \
 --entry-point gcc_calculate_tri_xtri_twap \
---runtime python37 \
+--runtime python39 \
 --trigger-topic update_tri_xtri_twap \
 --source https://source.developers.google.com/projects/trisolaris-ad-hoc/repos/github_trisolaris-labs_apr/moveable-aliases/master \
 --project trisolaris-ad-hoc
