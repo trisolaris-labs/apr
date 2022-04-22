00c968c8fd00bd84ef56134a0346b8b439c1e208
Baboo Bhaiya <baboobhaiya@protonmail.com>
2022-04-22 14:15:08 +0100

    delete unused files

--- heroku.py
+++
@@ -1,8 +0,0 @@
-from apr import apr
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-# Create an instance of scheduler and add function.
-scheduler = BlockingScheduler()
-scheduler.add_job(apr, "interval", seconds=120)
-
-scheduler.start()
