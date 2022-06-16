49f4b1ca28f8129024fbad33f9732848ceaabe23
Eric Juta <ericjohnjuta@gmail.com>
2022-06-16 10:15:34 +0100

    chore(setup.py, setup.cfg, pyproject.toml, .pre-commit-config.yaml): port over apeworx configs

--- README.md
+++ README.md
@@ -2,11 +2,25 @@
 
 This repo is used to perform offchain calculations, these calculations are deployed on GCP functions
 
+## Dependencies
+
+python3 version 3.7.2 or greater, python3-dev
+
+### Setup via setuptools
+
+You can clone the repository and use setuptools for the most up-to-date version:
+
+git clone https://github.com/trisolaris-labs/apr.git
+cd apr
+python3 -m venv venv
+pip install -e .
+
 ### How to add a new APR to cloud function
 
-The only thing that needs to be done to add a new pool for APR calculation is add a new dictionary in the  `V2_POOLS` array [here](https://github.com/trisolaris-labs/apr/blob/9934411f39dca9ca9391fb79768ba7b9e3d09fe9/utils/constants.py#L239)
+The only thing that needs to be done to add a new pool for APR calculation is add a new dictionary in the `V2_POOLS` array [here](https://github.com/trisolaris-labs/apr/blob/9934411f39dca9ca9391fb79768ba7b9e3d09fe9/utils/constants.py#L239)
 
-Sample 
+Sample
+
 ```
         23: {
             "LP": "0xBBf3D4281F10E537d5b13CA80bE22362310b2bf9",
@@ -22,10 +36,10 @@ Sample
             }
 ```
 
-* The first number signifies the ID of the pool in ChefV2
-* **LP**: key signifies the LP address which is being incentivized
-* **LPType**: signifies if its a StableAMM of not
-* **Rewarder**: signifies the rewarder address for v2 rewards, put a zero address fif there is no dual rewarder
-* **CoingeckoRewarderTokenName**: Is the coingecko token name of the rewarder token. This is to get the price of the rewarder token from Coingecko. Leave it empty if we need to get the price from the dex.
-* **RewarderPriceLP**: Is the LP address one of its tokens SHOULD be the rewarder token and the second should be one of either USDC, USDT, WNEAR or TRI. This LP will be used to determine the price of rewarder token if `CoingeckoRewarderTokenName` is empty.
-* **RewarderTokenDecimals**: Are the decimals in the rewarder token.
+- The first number signifies the ID of the pool in ChefV2
+- **LP**: key signifies the LP address which is being incentivized
+- **LPType**: signifies if its a StableAMM of not
+- **Rewarder**: signifies the rewarder address for v2 rewards, put a zero address fif there is no dual rewarder
+- **CoingeckoRewarderTokenName**: Is the coingecko token name of the rewarder token. This is to get the price of the rewarder token from Coingecko. Leave it empty if we need to get the price from the dex.
+- **RewarderPriceLP**: Is the LP address one of its tokens SHOULD be the rewarder token and the second should be one of either USDC, USDT, WNEAR or TRI. This LP will be used to determine the price of rewarder token if `CoingeckoRewarderTokenName` is empty.
+- **RewarderTokenDecimals**: Are the decimals in the rewarder token.
---
+++ setup.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup  # type: ignore
+
+extras_require = {
+    "test": [  # `test` GitHub Action jobs uses this
+        "pytest>=6.0,<7.0",  # Core testing package
+        "pytest-xdist",  # multi-process runner
+        "pytest-cov",  # Coverage analyzer plugin
+        "hypothesis>=6.2.0,<7.0",  # Strategy-based fuzzer
+        "eth-hash[pysha3]",  # For eth-utils address checksumming
+    ],
+    "lint": [
+        "black>=22.3.0,<23.0",  # auto-formatter and linter
+        "mypy>=0.960,<1.0",  # Static type analyzer
+        "flake8>=4.0.1,<5.0",  # Style linter
+        "isort>=5.10.1,<6.0",  # Import sorting linter
+    ],
+    "dev": [
+        "commitizen",  # Manage commits and publishing releases
+        "pre-commit",  # Ensure that linters are run prior to commiting
+        "pytest-watch",  # `ptw` test watcher/runner
+        "IPython",  # Console for interacting
+        "ipdb",  # Debugger (Must use `export PYTHONBREAKPOINT=ipdb.set_trace`)
+    ],
+}
+
+# NOTE: `pip install -e .[dev]` to install package
+extras_require["dev"] = (
+    extras_require["test"]
+    + extras_require["lint"]
+    + extras_require["dev"]
+)
+
+with open("./README.md") as readme:
+    long_description = readme.read()
+
+
+setup(
+    name="apr",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    description="""apr: Trisolaris APR calculating serverless functions""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Trisolaris DAO",
+    author_email="0xchain@trisolaris.io",
+    url="https://github.com/trisolaris-labs/apr",
+    include_package_data=True,
+    install_requires=[
+        "importlib-metadata ; python_version<'3.8'",
+        "pydantic>=1.9.0,<2.0",
+        "hexbytes>=0.2.2,<1.0.0",
+        "eth-utils>=1.10.0",
+        "APScheduler==3.8.1",
+        "eth_account==0.5.6",
+        "google-cloud-storage==1.43.0",
+        "protobuf==3.19.1",
+        "web3==5.25.0",
+        "retry==0.9.2",
+    ],  # NOTE: Add 3rd party libraries here
+    python_requires=">=3.7.2,<4",
+    extras_require=extras_require,
+    py_modules=[""],
+    license="Apache-2.0",
+    zip_safe=False,
+    keywords="ethereum",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    package_data={},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
