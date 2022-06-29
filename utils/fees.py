177f9d5ddf026c68c0d4dacfa5b510a99d6f5e3a
specialist0x <94581898+specialist0x@users.noreply.github.com>
2022-06-28 20:41:20 -0700

    Added logging to convertStablestoLP and convertFeesForPair

--- utils/fees.py
+++ utils/fees.py
@@ -30,6 +30,7 @@ def convertFeesForPair(tri_maker, pair, w3, acct):
         signed = w3.eth.account.sign_transaction(convert_tranasction, acct.key)
         signed_txn = w3.eth.sendRawTransaction(signed.rawTransaction)
         txn_hash = signed_txn.hex()
+        gccPrint(f"[convertFeesForPair] TX Hash: " + txn_hash)
         receipt = w3.eth.waitForTransactionReceipt(txn_hash, timeout=1200)
         for l in receipt["logs"]:
             if (
@@ -110,6 +111,7 @@ def convertStablestoLP(stable_lp_maker_v2, w3, acct):
         signed = w3.eth.account.sign_transaction(convert_tranasction, acct.key)
         signed_txn = w3.eth.sendRawTransaction(signed.rawTransaction)
         txn_hash = signed_txn.hex()
+        gccPrint("[convertStablestoLP] TX Hash: " + txn_hash)
         receipt = w3.eth.waitForTransactionReceipt(txn_hash, timeout=1200)
         for l in receipt["logs"]:
             # checks for LogLpTokensSentTopTRI event
