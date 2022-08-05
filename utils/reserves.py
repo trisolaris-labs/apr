6c109cbeaf19f7bcb3bbf150b71ab6fd0713649d
specialist0x <94581898+specialist0x@users.noreply.github.com>
2022-08-05 10:18:53 -0700

    Added cg token price fetch for stNEAR

--- utils/reserves.py
+++ utils/reserves.py
@@ -1,11 +1,7 @@
-from .node import (
-    init_erc20, 
-    init_stable_pool, 
-    init_tlp,
-    init_rewarder
-)
+from .node import init_erc20, init_stable_pool, init_tlp, init_rewarder
 from .constants import (
-    USDC_ADDRESS, 
+    STNEAR_ADDRESS,
+    USDC_ADDRESS,
     USDT_ADDRESS,
     WNEAR_ADDRESS,
     WETH_ADDRESS,
@@ -17,6 +13,7 @@ from .constants import (
     V2_STABLEPOOL_SWAP_CONTRACT,
 )
 from .prices import (
+    getCoingeckoUSDPriceRatio,
     getTriXTriRatio,
     getTokenUSDRatio,
 )
@@ -27,78 +24,99 @@ from .prices import (
 def getReserveStables(w3, total_supply, address):
     stable_pool_contract = init_stable_pool(address)
     virtual_price = stable_pool_contract.functions.getVirtualPrice().call()
-    reserve = (virtual_price * total_supply)/(10**36)
+    reserve = (virtual_price * total_supply) / (10**36)
     return reserve
+
 
 def getReserveInUsd(w3, tlp, triUsdRatio, wnearUsdRatio, wethUsdRatio):
     t0 = tlp.functions.token0().call()
     t1 = tlp.functions.token1().call()
     reserves = tlp.functions.getReserves().call()
-    if (t0 == USDC_ADDRESS or t1 == USDC_ADDRESS):
+    if t0 == USDC_ADDRESS or t1 == USDC_ADDRESS:
         decimals = init_erc20(USDC_ADDRESS).functions.decimals().call()
         if t0 == USDC_ADDRESS:
-            return reserves[0]*2/10**decimals
+            return reserves[0] * 2 / 10**decimals
         else:
-            return reserves[1]*2/10**decimals
-    elif (t0 == WNEAR_ADDRESS or t1 == WNEAR_ADDRESS):
+            return reserves[1] * 2 / 10**decimals
+    elif t0 == WNEAR_ADDRESS or t1 == WNEAR_ADDRESS:
         decimals = init_erc20(WNEAR_ADDRESS).functions.decimals().call()
         if t0 == WNEAR_ADDRESS:
-            reserveInWnear = reserves[0]*2/10**decimals
+            reserveInWnear = reserves[0] * 2 / 10**decimals
         else:
-            reserveInWnear = reserves[1]*2/10**decimals
-        return reserveInWnear/wnearUsdRatio
-    elif (t0 == WETH_ADDRESS or t1 == WETH_ADDRESS):
+            reserveInWnear = reserves[1] * 2 / 10**decimals
+        return reserveInWnear / wnearUsdRatio
+    elif t0 == WETH_ADDRESS or t1 == WETH_ADDRESS:
         decimals = init_erc20(WETH_ADDRESS).functions.decimals().call()
         if t0 == WETH_ADDRESS:
-            reserveInWeth = reserves[0]*2/10**decimals
+            reserveInWeth = reserves[0] * 2 / 10**decimals
         else:
-            reserveInWeth = reserves[1]*2/10**decimals
-        return reserveInWeth/wethUsdRatio
-    elif (t0 == TRI_ADDRESS or t1 == TRI_ADDRESS ):
+            reserveInWeth = reserves[1] * 2 / 10**decimals
+        return reserveInWeth / wethUsdRatio
+    elif t0 == TRI_ADDRESS or t1 == TRI_ADDRESS:
         decimals = init_erc20(TRI_ADDRESS).functions.decimals().call()
         if t0 == TRI_ADDRESS:
-            reserveInTri = reserves[0]*2/10**decimals
+            reserveInTri = reserves[0] * 2 / 10**decimals
         else:
-            reserveInTri = reserves[1]*2/10**decimals
-        return reserveInTri/triUsdRatio
-    elif (t0 == TRIBAR_ADDRESS or t1 == TRIBAR_ADDRESS ):
+            reserveInTri = reserves[1] * 2 / 10**decimals
+        return reserveInTri / triUsdRatio
+    elif t0 == TRIBAR_ADDRESS or t1 == TRIBAR_ADDRESS:
         decimals = init_erc20(TRIBAR_ADDRESS).functions.decimals().call()
         if t0 == TRIBAR_ADDRESS:
-            reserveInXTri = reserves[0]*2/10**decimals
+            reserveInXTri = reserves[0] * 2 / 10**decimals
         else:
-            reserveInXTri = reserves[1]*2/10**decimals
-        return reserveInXTri*getTriXTriRatio(w3)/triUsdRatio
-    elif (t0 == XNL_ADDRESS or t1 == XNL_ADDRESS ):
+            reserveInXTri = reserves[1] * 2 / 10**decimals
+        return reserveInXTri * getTriXTriRatio(w3) / triUsdRatio
+    elif t0 == XNL_ADDRESS or t1 == XNL_ADDRESS:
         decimals = init_erc20(XNL_ADDRESS).functions.decimals().call()
         if t0 == XNL_ADDRESS:
-            reserveInTri = reserves[0]*2/10**decimals
+            reserveInTri = reserves[0] * 2 / 10**decimals
         else:
-            reserveInTri = reserves[1]*2/10**decimals
-        return reserveInTri/triUsdRatio
-    elif (t0 == USDT_ADDRESS or t1 == USDT_ADDRESS):
+            reserveInTri = reserves[1] * 2 / 10**decimals
+        return reserveInTri / triUsdRatio
+    elif t0 == USDT_ADDRESS or t1 == USDT_ADDRESS:
         decimals = init_erc20(USDT_ADDRESS).functions.decimals().call()
         if t0 == USDT_ADDRESS:
-            return reserves[0]*2/10**decimals
+            return reserves[0] * 2 / 10**decimals
         else:
-            return reserves[1]*2/10**decimals
-    
+            return reserves[1] * 2 / 10**decimals
+    elif t0 == STNEAR_ADDRESS or t1 == STNEAR_ADDRESS:
+        decimals = init_erc20(STNEAR_ADDRESS).functions.decimals().call()
+        stnear_usd_ratio = getCoingeckoUSDPriceRatio("staked-near")
+
+        if t0 == STNEAR_ADDRESS:
+            reserveInTri = reserves[0] * 2 / 10**decimals
+        else:
+            reserveInTri = reserves[1] * 2 / 10**decimals
+        return reserveInTri / stnear_usd_ratio
+
 
 def getTotalStakedInUSD(totalStaked, totalAvailable, reserveInUSD):
     if totalAvailable == 0:
         return 0
     else:
-        return totalStaked*reserveInUSD/totalAvailable
+        return totalStaked * reserveInUSD / totalAvailable
 
 
 def getAPR(tokenUsdRatio, totalRewardRate, totalStakedInUSD):
-    if (totalStakedInUSD == 0 or tokenUsdRatio == 0):
+    if totalStakedInUSD == 0 or tokenUsdRatio == 0:
         return 0
     else:
         totalYearlyRewards = totalRewardRate * 3600 * 24 * 365
-        return totalYearlyRewards*100/(totalStakedInUSD*tokenUsdRatio)
+        return totalYearlyRewards * 100 / (totalStakedInUSD * tokenUsdRatio)
 
 
-def getDataV1Pools(w3, id, address, chef, triPerBlock, totalAllocPoint, tri_decimals, triUsdRatio, wnearUsdRatio, wethUsdRatio):
+def getDataV1Pools(
+    w3,
+    id,
+    address,
+    chef,
+    triPerBlock,
+    totalAllocPoint,
+    tri_decimals,
+    triUsdRatio,
+    wnearUsdRatio,
+    wethUsdRatio,
+):
     tlp = init_tlp(address)
     poolInfo = chef.functions.poolInfo(id).call()
     assert poolInfo[0].lower() == address.lower()
@@ -108,47 +126,58 @@ def getDataV1Pools(w3, id, address, chef, triPerBlock, totalAllocPoint, tri_deci
     totalStaked = tlp.functions.balanceOf(chef.address).call()
     totalStakedInUSD = getTotalStakedInUSD(totalStaked, totalSupply, reserveInUSDC)
     totalSecondRewardRate = (
-        triPerBlock * allocPoint / (totalAllocPoint * 10 ** tri_decimals)
-    )  
-    totalWeeklyRewardRate = (
-        3600 * 24 * 7 * totalSecondRewardRate
+        triPerBlock * allocPoint / (totalAllocPoint * 10**tri_decimals)
     )
+    totalWeeklyRewardRate = 3600 * 24 * 7 * totalSecondRewardRate
     return {
-                "id": id,
-                "poolId": id,
-                "lpAddress": address,
-                "totalSupply": totalSupply,
-                "totalStaked": totalStaked,
-                "totalStakedInUSD": totalStakedInUSD,
-                "totalRewardRate": totalWeeklyRewardRate,
-                # "totalWeeklyRewardRate": totalWeeklyRewardRate,
-                "allocPoint": allocPoint,
-                "apr": getAPR(triUsdRatio, totalSecondRewardRate, totalStakedInUSD),
-                "nonTriAPRs": [],
-                "chefVersion": "v1",
-            }
+        "id": id,
+        "poolId": id,
+        "lpAddress": address,
+        "totalSupply": totalSupply,
+        "totalStaked": totalStaked,
+        "totalStakedInUSD": totalStakedInUSD,
+        "totalRewardRate": totalWeeklyRewardRate,
+        # "totalWeeklyRewardRate": totalWeeklyRewardRate,
+        "allocPoint": allocPoint,
+        "apr": getAPR(triUsdRatio, totalSecondRewardRate, totalStakedInUSD),
+        "nonTriAPRs": [],
+        "chefVersion": "v1",
+    }
 
-def getDataV2Pools(w3, id, pool, chefv2, dummyLpTotalSecondRewardRate, totalAllocPointV2, triUsdRatio, wnearUsdRatio, wethUsdRatio):
+
+def getDataV2Pools(
+    w3,
+    id,
+    pool,
+    chefv2,
+    dummyLpTotalSecondRewardRate,
+    totalAllocPointV2,
+    triUsdRatio,
+    wnearUsdRatio,
+    wethUsdRatio,
+):
     tlp = init_tlp(pool["LP"])
     poolInfo = chefv2.functions.poolInfo(id).call()
     allocPoint = poolInfo[2]
     totalSupply = tlp.functions.totalSupply().call()
     totalStaked = tlp.functions.balanceOf(chefv2.address).call()
     totalSecondRewardRate = (
-            dummyLpTotalSecondRewardRate * allocPoint / (totalAllocPointV2)
+        dummyLpTotalSecondRewardRate * allocPoint / (totalAllocPointV2)
     )  # Taking TRI allocation to dummy LP in chef v1 as tri per block for chef V2
-    totalWeeklyRewardRate = (
-        3600 * 24 * 7 * totalSecondRewardRate
-    )
+    totalWeeklyRewardRate = 3600 * 24 * 7 * totalSecondRewardRate
 
     # Stable AMM LP staked amts logic
     if pool["LPType"] == "StableAMM":
-        stable_pool_contract = init_stable_pool(V2_STABLEPOOL_SWAP_CONTRACT[id]["poolContract"])
+        stable_pool_contract = init_stable_pool(
+            V2_STABLEPOOL_SWAP_CONTRACT[id]["poolContract"]
+        )
         virtual_price = stable_pool_contract.functions.getVirtualPrice().call()
-        totalStakedInUSDC = (virtual_price/1e18) * (totalStaked/1e18)
+        totalStakedInUSDC = (virtual_price / 1e18) * (totalStaked / 1e18)
     else:
         # Normal AMM LP staked amts logic
-        reserveInUSDC = getReserveInUsd(w3, tlp, triUsdRatio, wnearUsdRatio, wethUsdRatio)
+        reserveInUSDC = getReserveInUsd(
+            w3, tlp, triUsdRatio, wnearUsdRatio, wethUsdRatio
+        )
         totalStakedInUSDC = getTotalStakedInUSD(totalStaked, totalSupply, reserveInUSDC)
 
     nonTriAPRs = []
@@ -159,28 +188,37 @@ def getDataV2Pools(w3, id, pool, chefv2, dummyLpTotalSecondRewardRate, totalAllo
         doubleRewardUsdRatioForItem = 0
         rewarderContractForItem = init_rewarder(pool["Rewarder"])
         rewardDecimalsForItem = pool["RewarderTokenDecimals"]
-        rewardsPerBlockForItem = rewarderContractForItem.functions.tokenPerBlock().call()/(10**rewardDecimalsForItem)
+        rewardsPerBlockForItem = (
+            rewarderContractForItem.functions.tokenPerBlock().call()
+            / (10**rewardDecimalsForItem)
+        )
         rewarderAddressForItem = rewarderContractForItem.functions.rewardToken().call()
         print(f"Double rewards per block: {rewardsPerBlockForItem}")
-        doubleRewardUsdRatioForItem = getTokenUSDRatio(w3, pool, rewarderAddressForItem, wnearUsdRatio, triUsdRatio)
+        doubleRewardUsdRatioForItem = getTokenUSDRatio(
+            w3, pool, rewarderAddressForItem, wnearUsdRatio, triUsdRatio
+        )
 
         nonTriAPRs.append(
             {
                 "address": rewarderAddressForItem,
-                "apr": getAPR(doubleRewardUsdRatioForItem, rewardsPerBlockForItem, totalStakedInUSDC),
+                "apr": getAPR(
+                    doubleRewardUsdRatioForItem,
+                    rewardsPerBlockForItem,
+                    totalStakedInUSDC,
+                ),
             }
         )
 
     return {
-                "id": len(V1_POOLS) + id,
-                "poolId": id,
-                "lpAddress": pool["LP"],
-                "totalSupply": totalSupply,
-                "totalStaked": totalStaked,
-                "totalStakedInUSD": totalStakedInUSDC,
-                "totalRewardRate": totalWeeklyRewardRate,
-                "allocPoint": allocPoint,
-                "apr": getAPR(triUsdRatio, totalSecondRewardRate, totalStakedInUSDC),
-                "nonTriAPRs": nonTriAPRs,
-                "chefVersion": "v2",
-            }
+        "id": len(V1_POOLS) + id,
+        "poolId": id,
+        "lpAddress": pool["LP"],
+        "totalSupply": totalSupply,
+        "totalStaked": totalStaked,
+        "totalStakedInUSD": totalStakedInUSDC,
+        "totalRewardRate": totalWeeklyRewardRate,
+        "allocPoint": allocPoint,
+        "apr": getAPR(triUsdRatio, totalSecondRewardRate, totalStakedInUSDC),
+        "nonTriAPRs": nonTriAPRs,
+        "chefVersion": "v2",
+    }
