9adcc34a015db195a2cc03ad6c4d5a67bb26c7ef
specialist0x <94581898+specialist0x@users.noreply.github.com>
2022-08-05 17:46:27 -0700

    Updated Covalent API response pagination

--- utils/covalent.py
+++ utils/covalent.py
@@ -4,7 +4,7 @@ import requests
 from retry import retry
 
 COVALENT_API_KEY = os.getenv("COVALENT_API_KEY", "")
-COVALENT_ENDPOINT = f"https://api.covalenthq.com/v1/1313161554/xy=k/trisolaris/pools/?quote-currency=USD&format=JSON&key={COVALENT_API_KEY}"
+COVALENT_ENDPOINT = f"https://api.covalenthq.com/v1/1313161554/xy=k/trisolaris/pools/?quote-currency=USD&format=JSON&key={COVALENT_API_KEY}&page-size=1000"
 
 
 @retry((Exception), delay=10, tries=5)
