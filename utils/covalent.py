0a69916187c257525c476e72bae6ea0853e323ca
specialist0x <94581898+specialist0x@users.noreply.github.com>
2022-08-05 17:39:58 -0700

    Added gcloud logging to covalent api response

--- utils/covalent.py
+++ utils/covalent.py
@@ -1,3 +1,4 @@
+from gcc_utils import gccPrint
 import os
 import requests
 from retry import retry
@@ -5,13 +6,16 @@ from retry import retry
 COVALENT_API_KEY = os.getenv("COVALENT_API_KEY", "")
 COVALENT_ENDPOINT = f"https://api.covalenthq.com/v1/1313161554/xy=k/trisolaris/pools/?quote-currency=USD&format=JSON&key={COVALENT_API_KEY}"
 
+
 @retry((Exception), delay=10, tries=5)
 def getPools():
     pools = []
     try:
         response = requests.get(COVALENT_ENDPOINT).json()
-        pools = response['data']['items']
+        pools = response["data"]["items"]
+        gccPrint("[Covalent API::getPools] Success")
     except Exception as e:
+        gccPrint("[Covalent API::getPools] Error", "ERROR")
         raise e
 
     return pools
