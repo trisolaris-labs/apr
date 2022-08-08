9fe2e3401a87a689743e477e39f7c67fb02a4ba7
specialist0x <94581898+specialist0x@users.noreply.github.com>
2022-08-08 15:59:18 -0700

    Added covalent pools call

--- utils/covalent.py
+++ utils/covalent.py
@@ -5,13 +5,31 @@ from retry import retry
 COVALENT_API_KEY = os.getenv("COVALENT_API_KEY", "")
 COVALENT_ENDPOINT = f"https://api.covalenthq.com/v1/1313161554/xy=k/trisolaris/pools/?quote-currency=USD&format=JSON&key={COVALENT_API_KEY}"
 
+
 @retry((Exception), delay=10, tries=5)
 def getPools():
     pools = []
     try:
         response = requests.get(COVALENT_ENDPOINT).json()
-        pools = response['data']['items']
+        pools = response["data"]["items"]
     except Exception as e:
         raise e
 
     return pools
+
+
+@retry((Exception), delay=10, tries=5)
+def getPool(address):
+    result = None
+    try:
+        response = requests.get(
+            f"https://api.covalenthq.com/v1/1313161554/xy=k/trisolaris/pools/address/{address}/?quote-currency=USD&format=JSON&key={COVALENT_API_KEY}"
+        ).json()
+        results = response["data"]["items"]
+
+        if len(results) > 0:
+            result = results[0]
+    except Exception as e:
+        raise e
+
+    return result
